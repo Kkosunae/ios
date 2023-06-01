6b7dd5d281784967e3449a3d772b7fa83a22f479
Harry <jeongus97@gmail.com>
2023-06-01 19:38:19 +0900

    Init: 프로젝트 생성 (#1)
    * SwiftUI 프로젝트 생성

---
+++ kkosunae/kkosunaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  kkosunaeApp.swift
+//  kkosunae
+//
+//  Created by 전현성 on 2023/06/01.
+//
+
+import SwiftUI
+
+@main
+struct kkosunaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
