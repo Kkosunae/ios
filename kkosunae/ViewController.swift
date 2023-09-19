3ee91bd10ea7bb76b59dac9ceb03107a2d9730f1
Harry <jeongus97@gmail.com>
2023-09-19 21:13:20 +0900

    [KKO-113] Created a new project

---
+++ kkosunae/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kkosunae
+//
+//  Created by 전현성 on 2023/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
